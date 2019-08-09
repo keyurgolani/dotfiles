a4b661136964a03410ed654fa8907f5349d9e5b4
Keyur Golani <keyurrgolani@gmail.com>
2019-08-09 16:49:48 -0700

    Adding iterm preferences to dotfiles

--- setup/installs/iterm.sh
+++ setup/installs/iterm.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 brew cask install iterm2
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/.dotfiles/dotfiles/preferences/iterm"
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
