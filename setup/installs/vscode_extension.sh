f07338f5495533524efa86c542998446c27168c5
Keyur Golani <keyurrgolani@gmail.com>
2019-08-12 15:13:46 -0700

    Fix on a VS Code Extension name

--- setup/installs/vscode_extension.sh
+++ setup/installs/vscode_extension.sh
@@ -8,7 +8,7 @@ code --install-extension equinusocio.vsc-material-theme --force
 code --install-extension eg2.vscode-npm-script --force
 code --install-extension christian-kohler.path-intellisense --force
 code --install-extension ms-python.python --force
-code --install-extension visualstudioexptteam.vscodein --force
+code --install-extension visualstudioexptteam.vscodeintellicode --force
 code --install-extension vscode-icons-team.vscode-icons --force
 code --install-extension redhat.java --force
 code --install-extension ms-azuretools.vscode-docker --force
