4e49602bc721e43c2838024f1699aae95c6aec63
Keyur Golani <keyurrgolani@gmail.com>
2019-08-16 00:20:06 -0700

    Fixing homebrew silent installation and commandline tools popup

--- install.sh
+++ install.sh
@@ -32,6 +32,15 @@ if [ -d "$DIRECTORY" ]; then
     exit 1
 fi
 
+touch /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress;
+PROD=$(softwareupdate -l |
+ grep "\*.*Command Line.*$(sw_vers -productVersion|awk -F. '{print $1"."$2}')" |
+ head -n 1 | awk -F"*" '{print $2}' |
+ sed -e 's/^ *//' |
+ tr -d '\n')
+softwareupdate -i "$PROD" --verbose
+rm /tmp/.com.apple.dt.CommandLineTools.installondemand.in-progress
+
 echo "Checking out dotfiles..."
 
 git clone https://github.com/keyurgolani/dotfiles.git $DIRECTORY
