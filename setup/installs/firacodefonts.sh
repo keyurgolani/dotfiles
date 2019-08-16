fef758365bc69fe34322f3f94b4ab364bc26979d
Keyur Golani <keyurrgolani@gmail.com>
2019-08-15 21:36:13 -0700

    Fixing brew tap conflict issue

--- setup/installs/firacodefonts.sh
+++ setup/installs/firacodefonts.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-brew tap caskroom/fonts
 brew cask install font-fira-code
--- setup/installs/nerdfonts.sh
+++ setup/installs/nerdfonts.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 
-brew tap homebrew/cask-fonts
 brew cask install $(brew search nerd-font | sed '1d;')
